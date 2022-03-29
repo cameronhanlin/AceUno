6247146ef88151d683c05e4342047f83d18c0318
Andrea Santos <andreasantos@macbook-pro.myfiosgateway.com>
2022-03-29 11:13:45 -0400

    Card error for player two and three

--- src/main/java/AceUno/controller/GameController.java
+++ src/main/java/AceUno/controller/GameController.java
@@ -21,6 +21,12 @@ public class GameController {
 
     @RequestMapping("/player1/{cardID}")
     public String displayPlayer1(@PathVariable int cardID, ModelMap modelmap) {
+        if (cardID == 98){
+            dealer.drawCard(3);
+        }
+        if (cardID >=0 && cardID <=55){
+            dealer.discardCard(3, cardID);
+        }
         modelmap.put("player1Hand", dealer.getPlayerHand(1));
         modelmap.put("topDiscardCard", dealer.getTopDiscardCard());
         return "player1";
@@ -29,12 +35,28 @@ public class GameController {
 
     @RequestMapping("/player2/{cardID}")
     public String displayPlayer2(@PathVariable int cardID, ModelMap modelmap) {
+        if (cardID == 98){
+            dealer.drawCard(1);
+        }
+        if (cardID >=0 && cardID <=55){
+            dealer.discardCard(1, cardID);
+        }
+        modelmap.put("player2Hand", dealer.getPlayerHand(2));
+        modelmap.put("topDiscardCard", dealer.getTopDiscardCard());
         return "player2";
     }
 
 
     @RequestMapping("/player3/{cardID}")
     public String displayPlayer3(@PathVariable int cardID, ModelMap modelmap) {
+        if (cardID == 98){
+            dealer.drawCard(2);
+        }
+        if (cardID >=0 && cardID <=55){
+            dealer.discardCard(2, cardID);
+        }
+        modelmap.put("player3Hand", dealer.getPlayerHand(3));
+        modelmap.put("topDiscardCard", dealer.getTopDiscardCard());
         return "player3";
     }
 
